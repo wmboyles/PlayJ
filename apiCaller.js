bd9ff7aceee3f75463dd4c25a3994924dc1c33bc
Savan Doshi <SevenYoshisDoshi@gmail.com>
2020-05-02 20:36:30 -0400

    Update apiCaller.js

--- apiCaller.js
+++ apiCaller.js
@@ -11,3 +11,22 @@ more API calls are made for the duration of the game.
 // TODO: a function that selects 6 different categories, each with questions that have the right dollar amounts.
 
 // TODO: a function that takes an object from create.js and fills in the information for the corresponding question.
+var cat_req = new XMLHttpRequest();
+cat_req.open('GET', 'http://jservice.io/api/categories?count=6', false)
+cat_req.send();
+var arr = JSON.parse(cat_req.response);
+
+var categories = []
+var clues = [];
+arr.forEach(cat => {
+  categories.push(cat.title);
+  var clue_req = new XMLHttpRequest();
+  clue_req.open("GET", "http://jservice.io/api/clues?category="+cat.id, false);
+  clue_req.send();
+  var arr2 = JSON.parse(clue_req.response);
+  for(var i = 0; i < 5; i++){
+    clues.push(arr2[i]); 
+  }
+});
+console.log(categories);
+console.log(clues);
